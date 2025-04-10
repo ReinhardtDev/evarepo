d8ec71161e2c0771c76f11e3e04557e65dd6a969
Oliver Reinhardt <oliverreinhardt@outlook.de>
2025-04-10 13:42:41 +0200

    commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main { //Haupt Klasse von der der Code läuft
+    public static void main(String[] args) { //Main Methode, die einen Array von Strings nimmt
+        System.out.println("Hello world!"); //schreibt "Hello World" in die Konsole
+    }
+}
