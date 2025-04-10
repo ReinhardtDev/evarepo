02b4477d9f0661e0b6089674cfc1d6a35ce8da06
Oliver Reinhardt <oliverreinhardt@outlook.de>
2025-04-10 14:18:35 +0200

    Hurra Events

--- src/Events.java
+++ src/Events.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Events {
+public class Events {
+
+    public String Bezeichnung;
+    public String Ort;
+    public String Datum;
+    public int Kontingent;
+
+    public Events(String bezeichnung, String ort, String datum, int kontingent) {
+        Bezeichnung = bezeichnung;
+        Ort = ort;
+        Datum = datum;
+        Kontingent = kontingent;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main { //Haupt Klasse von der der Code läuft
+
     public static void main(String[] args) { //Main Methode, die einen Array von Strings nimmt
-        System.out.println("Hello world!"); //schreibt "Hello World" in die Konsole
+        Events event = new Events("Event 1", "Leipzig", "11.11.2026", 27);
+        Events event2 = new Events("Event 2", "Leipzig", "34.8.3012", 68);
+        Events event3 = new Events("Event 3", "Dresden", "17.05.1002", 0);
+        System.out.println(event.Bezeichnung + ":" + "\n Datum: " + event.Datum + "\n Ort: " + event.Ort + "\n Ticketkontingent: " + event.Kontingent);
+        System.out.println(event2.Bezeichnung + ":" + "\n Datum: " + event2.Datum + "\n Ort: " + event2.Ort + "\n Ticketkontingent: " + event2.Kontingent);
+        System.out.println(event3.Bezeichnung + ":" + "\n Datum: " + event3.Datum + "\n Ort: " + event3.Ort + "\n Ticketkontingent: " + event3.Kontingent);
+
     }
 }
