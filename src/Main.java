543ff9ead459b8214bfdcf47cbbc0b364b18e4a6
Oliver Reinhardt <oliverreinhardt@outlook.de>
2025-04-24 13:54:59 +0200

    something

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,5 @@ public class Main {
         IDService idService = new IDService();
         Client client = new Client(idService);
         client.readUserChoice();
-        /*try {
-            EventService eventService = new EventService();
-            long buchmesseId = eventService.createEvent("Buchmesse", "Leipzig", LocalDate.parse("2026-03-29"), 1000);
-            long laternenfestId =  eventService.createEvent("Laternenfest", "Halle", LocalDate.parse("2025-08-29"), 2000);
-            long pokalfinaleID = eventService.createEvent("DFB-Pokalfinale", "Berlin", LocalDate.parse("2025-05-24"), 74475);
-
-
-
-            Event laternenfest = eventService.getEventById(laternenfestId);
-
-            laternenfest.setDate(LocalDate.parse("2025-08-30"));
-            laternenfest.setQuota(500);
-            eventService.updateEvent(laternenfest);
-
-            eventService.deleteEvent(buchmesseId);
-
-            for (Event e : eventService.getAllEvents())
-                System.out.println(e);
-
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }*/
     }
 }
